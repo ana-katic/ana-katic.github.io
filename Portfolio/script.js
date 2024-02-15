fa907badf8b28ac5b341706567dda92a1f50a6d5
Ana <ane.katic@hotmail.com>
2024-02-15 21:13:19 +0100

    Revert "Portfolio (grid layout, flex box)"
    This reverts commit a2f1d8b6d16b89238035060535bb647f814f798e.

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,43 +1,6 @@
-// --> button btn-mobile-nav
-
-const btnNavEl = document.querySelector('.btn-mobile-nav');
-
-// --> to the header
-
-const headerEL = document.querySelector('.header');
-
-btnNavEl.addEventListener('click', function() {
-  headerEL.classList.toggle('nav-open');
-});
-
-
-
-//  smooth scrolling animation 
-
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEL.classList.toggle('nav-open');
-    // here we are removing that nav-open iif it is on the header
-  });
-});
-
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
